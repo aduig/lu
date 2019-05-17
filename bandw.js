5f43dde638aa0cec190efc370d2793ad2be2084d
aduig <50614734+aduig@users.noreply.github.com>
2019-05-16 23:08:05 -0500

    Create bandw.js

---
+++ bandw.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff"]);
+t.prefs_.set('foreground-color', "#ffffff");
+t.prefs_.set('background-color', "#000000");
+t.prefs_.set('cursor-color', '#ffffff');
